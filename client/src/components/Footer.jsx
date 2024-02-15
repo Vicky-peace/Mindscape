41afd884e03ea7e630e48fa7d0dcf36f1a1a13e1
vince-star-os <vstar1945@gmail.com>
2024-02-15 14:34:33 +0300

    first commit

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      
+      &copy; {2024}. All rights reserved.
+      
+    </footer>
+  )
+}
+
+export default Footer 
